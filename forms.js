3309cad720f2b70368d6f106773791928e694c29
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2022-01-05 15:54:58 +0800

    add styles and function to login form

---
+++ forms.js
@@ -0,0 +1,12 @@
+function openlogin(){
+  document.getElementById("login").style.display = "block";
+}
+
+function opensignup(){
+  document.getElementById("signup").style.display = "block";
+}
+
+function closeform(){
+  document.getElementById("login").style.display = "none";
+}
+
